69553ecf19a8c86b88fd3cb33aab444a9d536227
Roberto Jesus Garcia Pino <pino@Robertos-MacBook-Air.local>
2022-02-07 18:34:36 -0600

    Environment.py, first Task

--- environment.py
+++ environment.py
@@ -1,33 +1,33 @@
 from random import randint, choice
 MAX_ROOMS = 5
+print(randint(0, 1))
 
-
-class Environment:
-    __rooms = 0
-    __currentRoom = -1
-    __state = []
+# class Environment:
+#     __rooms = 0
+#     __currentRoom = -1
+#     __state = []
     
-    def __init__(self):
-        print()
-        self.__currentRoom = randint(1, self.__rooms)
-        # self.__rooms = randint(1, MAX_ROOMS)
+#     def __init__(self):
+#         print()
+#         self.__currentRoom = randint(1, self.__rooms)
+#         # self.__rooms = randint(1, MAX_ROOMS)
         
-        # for room in range(0, self.__rooms):
-        #     self.__state.append(choice([True, False]))
+#         # for room in range(0, self.__rooms):
+#         #     self.__state.append(choice([True, False]))
 
-    def show(self):
-        print(self.__rooms)
-        print(self.__currentRoom)
-        print(self.__state) # ??
-        print() * 3
+#     def show(self):
+#         print(self.__rooms)
+#         print(self.__currentRoom)
+#         print(self.__state) # ??
+#         print() * 3
 
-def main():
-    print()
-    env = Environment()
-    # env.show()
+# def main():
+#     print()
+#     env = Environment()
+#     # env.show()
 
-if __name__ == "__main__":
-    main()
-    main()
-    main()
+# if __name__ == "__main__":
+#     main()
+#     main()
+#     main()
         
